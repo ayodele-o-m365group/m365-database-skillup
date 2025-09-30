0c9676a0d833289509af4410285e7d98922a362a
Ayodele O <ayodele.o@m365connect.com>
2025-09-30 04:39:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from typing import Union,Annotated,Literal
+from fastapi import FastAPI,Query,Path,Body
+from enum import Enum
+from pydantic import BaseModel,Field
+import asyncio
+
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
